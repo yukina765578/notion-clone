e7c6d7bcfcbd658e84bc17dc16b575f1cada73b1
yukina765578 <garden.students@gmail.com>
2024-02-12 00:14:08 +0900

    20240212

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,18 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/themes-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Notion Clone",
   description: "A clone of Notion",
+  icons: {
+    icon: {
+      url: "/favicon.ico",
+    },
+  },
 };
 
 export default function RootLayout({
@@ -15,8 +21,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          storageKey="notionClone-theme-2"
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
