b316d39575880c6680955025443fa113afe02efd
yukina765578 <garden.students@gmail.com>
2024-02-29 02:07:51 +0900

    commit 2024/02/29

--- app/(main)/_components/document-list.tsx
+++ app/(main)/_components/document-list.tsx
@@ -1,8 +1,14 @@
 "use client";
 
+import { api } from "@/convex/_generated/api";
 import { Doc, Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
 import { useParams } from "next/navigation";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Item } from "./item";
+import { cn } from "@/lib/utils";
+import { FileIcon } from "lucide-react";
 
 interface DocumentListProps {
   parentDocumentId?: Id<"documents">;
@@ -16,5 +22,69 @@ export const DocumentList = ({
 }: DocumentListProps) => {
   const params = useParams();
   const router = useRouter();
-  return <div>DocumentList</div>;
+  const [expanded, setExpanded] = useState<Record<string, boolean>>({});
+
+  const onExpand = (documentId: string) => {
+    setExpanded((prevExpanded) => ({
+      ...prevExpanded,
+      [documentId]: !prevExpanded[documentId],
+    }));
+  };
+
+  const documents = useQuery(api.documents.getSideBar, {
+    parentDocument: parentDocumentId,
+  });
+
+  const onRedirect = (documentId: string) => {
+    router.push(`/document/${documentId}`);
+  };
+
+  if (documents === undefined) {
+    return (
+      <>
+        <Item.Skelton level={level} />
+        {level === 0 && (
+          <>
+            <Item.Skelton level={level} />
+            <Item.Skelton level={level} />
+          </>
+        )}
+      </>
+    );
+  }
+
+  return (
+    <>
+      <p
+        style={{
+          paddingLeft: level ? `${level * 12 + 25}px` : undefined,
+        }}
+        className={cn(
+          "hidden text-sm font-medium text-muted-foreground/80",
+          expanded && "last:block",
+          level === 0 && "hidden",
+        )}
+      >
+        No pages inside
+      </p>
+      {documents.map((document) => (
+        <div key={document._id}>
+          <Item
+            id={document._id}
+            onClick={() => onRedirect(document._id)}
+            lable={document.title}
+            icon={FileIcon}
+            documentIcon={document.icon}
+            active={params.documentId === document._id}
+            level={level}
+            onExpand={() => onExpand(document._id)}
+            expanded={expanded[document._id]}
+          />
+          {expanded[document._id] && (
+            <DocumentList parentDocumentId={document._id} level={level + 1} />
+          )}
+        </div>
+      ))}
+    </>
+  );
 };
